29db1de8dae7c7fa98a86e20ba6d39fb43e1a1ed
Ryan Connor <ryanwc13@gmail.com>
2016-03-27 12:54:30 +0700

    starter code from UCSD

---
+++ src/graph/CapGraph.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package graph;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * @author ryanwilliamconnor
+ * © 2016 Ryan William Connor
+ */
+public class CapGraph implements Graph {
+
+	/* (non-Javadoc)
+	 * @see graph.Graph#addVertex(int)
+	 */
+	@Override
+	public void addVertex(int num) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see graph.Graph#addEdge(int, int)
+	 */
+	@Override
+	public void addEdge(int from, int to) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see graph.Graph#getEgonet(int)
+	 */
+	@Override
+	public Graph getEgonet(int center) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see graph.Graph#getSCCs()
+	 */
+	@Override
+	public List<Graph> getSCCs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see graph.Graph#exportGraph()
+	 */
+	@Override
+	public HashMap<Integer, HashSet<Integer>> exportGraph() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
