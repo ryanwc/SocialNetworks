12d246165e79900c8001f3419d562ccb82e59dcc
Ryan Connor <ryanwc13@gmail.com>
2016-03-27 23:34:20 +0700

    added egonet test

--- src/application/GraphTest.java
+++ src/application/GraphTest.java
@@ -1,16 +1,54 @@
 package application;
 
+import java.util.HashMap;
+import java.util.List;
+
 import graph.CapGraph;
+import graph.Graph;
+import graph.Vertex;
 import util.GraphLoader;
 
 public class GraphTest {
 
 	public static void main(String[] args) {
 		
-		CapGraph testGraph = new CapGraph("Test");
+		//CapGraph testGraphOne = new CapGraph("Test One");
+		CapGraph smallTestGraph = new CapGraph("Small Test");
 		
-		GraphLoader.loadGraph(testGraph, "data/small_test_graph.txt");
+		//GraphLoader.loadGraph(testGraph, "data/facebook_ucsd.txt");
+		GraphLoader.loadGraph(smallTestGraph, "data/small_test_graph.txt");
 		
-		testGraph.printGraph();
+		//testGraph.printGraph();
+		
+		//CapGraph testTranspose = testGraph.getTranspose();
+		
+		//testTranspose.printGraph();
+		
+		//System.out.println();
+		
+		/*
+		List<Vertex> testList = testGraphOne.getVertices().get(8195).getOutEdges();
+		
+		//System.out.println(testList.size());
+		
+		for (Vertex vertex : testList) {
+			System.out.println(vertex.getID());
+		}
+		*/
+		
+		/*
+		//Egonet test
+		for (int vertexID : smallTestGraph.getVertices().keySet()) {
+			
+			Graph egonet = smallTestGraph.getEgonet(vertexID);
+			System.out.println("Egonet for " + vertexID + ":");
+			((CapGraph)egonet).printGraph();
+		}
+		//*/
+		
+		///*
+		//SCCs test
+		
+		//*/
 	}
 }
