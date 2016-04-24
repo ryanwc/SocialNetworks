0983f237bd654d3fb61021425b8fadfcb8540925
Ryan Connor <ryanwc13@gmail.com>
2016-04-25 00:15:29 +0700

    expand SCC testing (still no unit test though)

--- src/application/GraphTest.java
+++ src/application/GraphTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import graph.CapGraph;
 import graph.Graph;
 import graph.StackExchangeTopicGraph;
+import graph.UserNode;
 import graph.Vertex;
 import util.GraphLoader;
 
@@ -19,23 +20,39 @@ public class GraphTest {
 		//GraphLoader.loadGraph(testGraph, "data/facebook_ucsd.txt");
 		//GraphLoader.loadGraph(testGraph, "data/small_test_graph.txt");
 		
-		StackExchangeTopicGraph graph = new StackExchangeTopicGraph("Test Topic 1");
-		GraphLoader.populateStackExchangeTopicGraph(graph, "data/stack_exchange/TestGraph1/");
+		StackExchangeTopicGraph graph = new StackExchangeTopicGraph("Buddhism");
+		GraphLoader.populateStackExchangeTopicGraph(graph, "data/stack_exchange/buddhism.stackexchange.com/");
 		
 		/* Print test
 		graph.printGraph();
-		*/
-		
-		/* SCC test
-		List<Graph> SCCs = graph.getSCCs();
-		for (Graph SCC : SCCs) {
-			if (SCC instanceof StackExchangeTopicGraph) {
-				((StackExchangeTopicGraph) SCC).printGraph();
-			}
-		}
 		//*/
 		
-		///* egonet test
+		/* SCC test
+		// test how many users don't have any posts (and will therefore be their own SCC)
+		int unconnectedUsers = 0;
+		for (UserNode user : graph.getUsers().values()) {
+			if (user.getOutEdges().size() < 1) {
+				unconnectedUsers++;
+			}
+		}
+		System.out.println(graph.getTopic() + " has " + unconnectedUsers + " unconnected users "
+				+ "out of " + graph.getUsers().size() + " total users" );
+		List<Graph> SCCs = graph.getSCCs();
+		int biggestSCC = 0;
+		System.out.println(graph.getTopic() + " has " + SCCs.size() + " SCCs");
+		for (Graph SCC : SCCs) {
+			if (((StackExchangeTopicGraph)SCC).getVertices().size() > biggestSCC) {
+				biggestSCC = ((StackExchangeTopicGraph)SCC).getVertices().size();
+			};
+			if (((StackExchangeTopicGraph)SCC).getVertices().size() > 1) {
+				
+				((StackExchangeTopicGraph)SCC).printStats();
+			}
+		}
+		System.out.println("Biggest SCC has " + biggestSCC + " vertices");
+		//*/
+		
+		/* egonet test
 		((StackExchangeTopicGraph)graph.getEgonet(1)).printGraph();
 		//*/
 		
